fdc30855569e2349b9b2005a99bf8753a53ac6c3
Rustohi <rustohidev@gmail.com>
2025-05-27 17:33:39 -0700

    proper format

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -3,6 +3,8 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
+
+
 console.log(
   `✅ Finished! We've loaded our secret key securely, using an env file!`,
 );
